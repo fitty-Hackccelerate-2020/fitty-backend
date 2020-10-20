c2a229e5f7b0373cbd51c715a9a22786d9b2797a
Marvin9 <marvinduff97@gmail.com>
2020-10-20 19:33:57 +0530

    chore: ist timezone in logger

--- setup/logger.js
+++ setup/logger.js
@@ -1,8 +1,14 @@
 const winston = require('winston');
+const moment = require('moment-timezone');
+
+const customTimestamp = winston.format((info) => ({
+  ...info,
+  timestamp: moment().tz('Asia/Kolkata').format(),
+}));
 
 module.exports = winston.createLogger({
   format: winston.format.combine(
-    winston.format.timestamp(),
+    customTimestamp(),
     winston.format.json(),
     winston.format.printf(
       (info) => `\n${info.timestamp} [${info.level}]: ${info.message}`
